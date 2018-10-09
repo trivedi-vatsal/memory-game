b5dda946d35eef4de1b7b1acf51096fa3cb2e2ce
Vatsal Trivedi <trivedivatsal005@gmail.com>
2018-10-09 19:20:47 +0530

    Add project

---
+++ README.md
@@ -0,0 +1,25 @@
+# Memory Game Project
+## How to play
+1. Download or clone the repository on your machine.
+2. Extract the .zip file
+3. Open index.html in your browser or [Click here](https://vattytrivedi.github.io/memory-game)
+
+## Rules and Regulations
+1. You have to match cards with identical face.
+2. Your moves are counted out as you proceed the game.
+3. If your moves are equal to 16 then you will get 3 Stars.
+4. If your moves are greater than 16 and less than 25 then you will get 2 Stars.
+5. Else you will get 1 Star.
+6. You can also restart the game at any stage by clicking the restart button.
+
+## Scoring
+* 3 stars = 16 moves or less
+* 2 stars = 25 moves or less
+* 1 star = 25 moves or more
+
+## External libraries Used
+* jQuery
+* [Roboto Font](https://fonts.googleapis.com/css?family=Roboto)
+* [MDBootstrap](https://mdbootstrap.com/)
+* [SweetAlert2](https://github.com/sweetalert2/sweetalert2)
+
---
+++ js/app.js
@@ -0,0 +1,160 @@
+let min = 0, sec = 0, hours = 0;
+let stop = 0;
+
+window.onload = function () {
+    setInterval(function () {
+        if (stop !== 1) {
+            sec++;
+            if (sec === 60) {
+                min++;
+                sec = 0;
+            }
+            if (min === 60) {
+                hours++;
+                min = 0;
+                sec = 0;
+            }
+            $('.timer').html(hours + ':' + min + ':' + sec);
+            console.log(min);
+            console.log(sec);
+        }
+
+    }, 1000);
+};
+
+//Create a list for cards
+let cards = [];
+let cardsName = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
+let openCards = [];
+
+//Display the cards on the page
+$('.deck').each(function () {
+    $(this).find('li').each(function () {
+        cards.push($(this));
+    });
+});
+
+let temp = 0;
+
+cardsName = shuffle(cardsName);
+
+let cardNumber = 0;
+
+$('.deck').each(function () {
+    $(this).find('li').find('i').each(function () {
+        $(this).removeAttr('class');
+        $(this).addClass(cardsName[cardNumber]);
+        cardNumber++;
+    });
+});
+
+$('.deck').each(function () {
+    $(this).find('li').find('i').each(function () {
+        let tempClass = $($(cards[temp][0]).find('i')[0]).attr('class');
+        $(this).removeAttr('class');
+        $(this).addClass(tempClass);
+        temp++;
+    });
+});
+
+// Shuffle function
+function shuffle(array) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+let moves = 0, stars = 3;
+
+removeProperties = function (prop) {
+    setTimeout(function () {
+        prop.removeClass('show open animated pulse');
+        openCards[0].removeClass('show open animated pulse');
+        openCards = [];
+    }, 400);
+};
+
+showCard = function (clickEvent) {
+    clickEvent.on('click', function () {
+        moves++;
+        if (moves === 16) {
+
+        } else if (moves > 16 && moves <= 25) {
+            $('section ul li').hide();
+            $('section ul').append('<li><i class="fa fa-star"></i></li>');
+            $('section ul').append('<li><i class="fa fa-star"></i></li>');
+            stars = 2;
+        } else if (moves > 25) {
+            $('section ul li').hide();
+            $('section ul').append('<li><i class="fa fa-star"></i></li>');
+            stars = 1;
+        }
+        $('.moves').html(moves);
+        if ((openCards.length % 2) === 0) {
+            $(this).addClass('show open animated pulse');
+            $(this).off('click');
+            openCards.push($(this));
+        } else if (openCards.length !== 0) {
+            $(this).addClass('show open animated pulse');
+            let self = $(this);
+            for (let i = 0; i < openCards.length; i++) {
+                if (openCards[i].find('i').attr('class') === self.find('i').attr('class')) {
+                    self.removeClass('animated pulse');
+                    self.addClass('show match animated tada');
+                    openCards[i].removeClass('animated pulse');
+                    openCards[i].addClass('show match animated tada');
+                    console.log('match');
+                    $(this).off('click');
+                    openCards = [];
+                    break;
+                } else {
+                    self.addClass('show open animated pulse');
+                    removeProperties(self);
+                    openCards[0].on('click', showCard(openCards[0]));
+                    console.log('no match');
+                }
+            }
+        }
+        if ($('.deck').find('.match').length === 16) {
+            setTimeout(function () {
+                $('.deck').each(function () {
+                    swal({
+                        title: '!! Hurrah !!',
+                        type: 'success',
+                        text: 'Won the game. Moves used ' + moves + '. You have got ' + stars + ' Stars. Time taken ' + hours + ':' + min + ':' + sec + ' ',
+                        allowOutsideClick: false,
+                        showCancelButton: true,
+                        confirmButtonText: 'Play Again',
+                        confirmButtonColor: '#FFF',
+                        cancelButtonText: 'Close',
+                        cancelButtonColor: '#FFF'
+                    }).then(function () {
+                        location.reload();
+                    }, function (dismiss) {
+                        console.log('Yes');
+                    });
+
+                });
+            }, 300);
+            stop = 1;
+            $('.timer').hide();
+            $('.timer').html('0:0:0');
+            $('.timer').show();
+        }
+    });
+};
+
+for (let i = 0; i < cards.length; i++) {
+    cards[i].on('click', showCard(cards[i]));
+}
+
+$('.restart').on('click', function () {
+    location.reload();
+});
